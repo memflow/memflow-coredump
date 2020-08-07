d0a4f06c8e38c36248be06c6172648847c975b76
ko1N <ko1N1337@gmail.com>
2020-08-07 14:39:51 +0200

    Fixed install script

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 cargo build --release --all-features
 if [[ ! -d ~/.local/lib/memflow ]]; then
-    mkdir ~/.local/lib/memflow
+    mkdir -p ~/.local/lib/memflow
 fi
 cp target/release/libmemflow_coredump.so ~/.local/lib/memflow
