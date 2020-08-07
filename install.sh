d0a7abbb229f3f8b8f399665da6461fd751c97d2
ko1N <ko1N1337@gmail.com>
2020-08-07 14:50:43 +0200

    Added makefile and --system flag to install script

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 cargo build --release --all-features
-if [[ ! -d ~/.local/lib/memflow ]]; then
-    mkdir -p ~/.local/lib/memflow
+
+if [ ! -z "$1" ] && [ $1 = "--system" ]; then
+    if [[ ! -d /usr/local/lib/memflow ]]; then
+        sudo mkdir /usr/local/lib/memflow
+    fi
+    sudo cp target/release/libmemflow_coredump.so /usr/local/lib/memflow
+else
+    if [[ ! -d ~/.local/lib/memflow ]]; then
+        mkdir -p ~/.local/lib/memflow
+    fi
+    cp target/release/libmemflow_coredump.so ~/.local/lib/memflow
 fi
-cp target/release/libmemflow_coredump.so ~/.local/lib/memflow
