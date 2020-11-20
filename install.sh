788e040d15009ce6e6a79a5fbdf6b38dee311f7e
ko1N <ko1N1337@gmail.com>
2020-11-20 23:37:17 +0100

    Updated install script and removed nightly requirement for windows ci

--- install.sh
+++ install.sh
@@ -8,14 +8,18 @@ else
   FILENAME=target/release/libmemflow_coredump.so
 fi
 
+# install connector to system dir
 if [ ! -z "$1" ] && [ $1 = "--system" ]; then
+    echo "installing connector system-wide in /usr/lib/memflow"
     if [[ ! -d /usr/lib/memflow ]]; then
         sudo mkdir /usr/lib/memflow
     fi
     sudo cp $FILENAME /usr/lib/memflow
-else
-    if [[ ! -d ~/.local/lib/memflow ]]; then
-        mkdir -p ~/.local/lib/memflow
-    fi
-    cp $FILENAME ~/.local/lib/memflow
 fi
+
+# install connector in user dir
+echo "installing connector for user in ~/.local/lib/memflow"
+if [[ ! -d ~/.local/lib/memflow ]]; then
+    mkdir -p ~/.local/lib/memflow
+fi
+cp $FILENAME ~/.local/lib/memflow
